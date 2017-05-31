f30949fac59134aeb46b7cb71a55002be821f795
mchangrh <mchangrh@gmail.com>
2017-05-31 11:02:53 -0400

    Commented RandomGenerator.java

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 public class RandomGenerator{
-    public static void main (String[] args){
-        System.out.println(randomGenerator());
-    }
+    // Hit is successful
     public static boolean hitChance () {
         return (randomGenerator()>=0.8);
     }
+    // Checks if first scene is true
     public static boolean sceneChance (double firstCondition) {
-        return (randomGenerator()>firstCondition);
+        return (randomGenerator()<=firstCondition);
     }
+    // Random Generator
     public static double randomGenerator() {
         Random random = new Random();
         return random.nextDouble();
