a7eabbbade78a422ce399be41b854bd23cc80027
RaghavSrinivasan <sriraghav45@gmail.com>
2017-06-05 09:36:01 -0400

    Add files via upload

--- Events.java
+++ Events.java
@@ -4,18 +4,24 @@
 
 public abstract class Events
 {
-	final static double BASECHANCE = 0.1;
 	private double lootDrop;
 	private Player user;
 	private String description; //description of what is actually happening
 	private Enemy[] enemies; //pass in enemies from the event you are executing
 	private ItemDrop[] lootOptions; //pass in the potential item drops from the event
 	
+	public Events(Player user, Enemy[] enemies, ItemDrop[] lootOptions)
+	{
+		this.user = user;
+		this.enemies = enemies;
+		this.lootOptions = lootOptions;
+		description = "";
+	}
+
 	public boolean encounterEnemy()
 	{
 		Enemy monster = enemies[RandomGenerator.range(enemies.length - 1)]; //generate enemies to return to Events so they can use
-		Fight combat = new Fight(user, monster);
-		return combat.battle();
+		description = "YOU ENCOUNTER A " + monster + ". PREPARE FOR BATTLE";
 	}
 	
 	public Loot obtainLoot()
@@ -26,31 +32,9 @@ public abstract class Events
 		 {
 		 	itemsGained[i] = items[i].generageItem(); //if no item obtained, instance is a null
 		 }
+		 lootFound = new Loot(itemsGained);
+		 description = "YOU FIND " + lootFound;
 		 
-		 return (new Loot(itemsGained));
+		 return (lootFound);
 	}
-	
-	public boolean executeEvent()//supposed to trigger an encounter in the Dusky Path
-	{
-		if (RandomGenerator.trueFalse(lootDrop)) //checks if player receives loot or not
-		{
-			lootDrop = BASECHANCE //after obaining loot, loot chance reverts to base
-			user.receiveLoot(obtainLoot());
-			return true;
-		}
-		else
-		{
-			boolean living = encounterEnemy();
-			if (living)
-			{
-				lootDrop += 0.1; //after defeating enemy, loot chance goes up
-			}
-			else
-			{
-				lootDrop = BASECHANCE;//if dead, loot chance reverts to base
-			}
-			
-			return living;	
-		}
-	}	
 }
