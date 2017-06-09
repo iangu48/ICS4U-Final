52da31add64ddf2adff8501eea0c1b6105134eb3
iangu48 <iangu48@users.noreply.github.com>
2017-06-09 09:18:46 -0400

    Create ItemDrop.java

--- ItemDrop.java
+++ ItemDrop.java
@@ -20,10 +20,10 @@ public class ItemDrop
         Item newItem = null;
         if (RandomGenerator.trueFalse(chance)) { //If item is dropped
             int amount = RandomGenerator.range(min, max); // Calculate number to drop
-            if (drop instanceof Material) { // If instance of store
-                newItem = new Material(amount, id, name); //Give store of x amount
+            if (drop instanceof Material) { // If instance of material
+                newItem = new Material(amount, id, name); //Give material of x amount
             }
-            else if (drop instanceof Weapon) { //If drop is store
+            else if (drop instanceof Weapon) { //If drop is weapon
                 int strength = ((Weapon)drop).getStrength();
                 newItem = new Weapon(amount, id, name, strength); //Give weapon of x amount
             }
