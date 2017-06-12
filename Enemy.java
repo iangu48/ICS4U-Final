224b70a579566cd4a1fd1a4db236bc70eee85b7c
iangu48 <iangu48@users.noreply.github.com>
2017-06-12 10:34:53 -0400

    Create Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -2,7 +2,7 @@ public class Enemy extends Entity {       //This is an enemy entity
 	String name;                           //name of enemy entity
 	int damage;                            //damage enemy deals
 	Loot lootDrop;                   //array of items that defeated monsters drop
-	String description;                    //
+	String encounterText, fightText;                    //
 	
 	public Enemy() {
 		hpMax = 1;
@@ -12,12 +12,14 @@ public class Enemy extends Entity {       //This is an enemy entity
 		lootDrop = null;
 	} 
 	
-	public Enemy(int hp, String name, int damage, Loot drops) {
+	public Enemy(int hp, String name, int damage, Loot drops, String encounterText, String fightText) {
 		hpMax = hp;
 		hpLeft = hp;
 		this.name = name;
 		this.damage = damage;
 		lootDrop = drops;
+		this.encounterText = encounterText;
+		this.fightText = fightText;
 	}
 	
 	public void attack(Entity other) {
