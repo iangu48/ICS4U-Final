f55e62eab1e3b838499e049d1ae66ea3d4935766
RaghavSrinivasan <sriraghav45@gmail.com>
2017-06-13 10:11:31 -0400

    Create Enemy.java

--- DuskyPath/Enemy.java
+++ DuskyPath/Enemy.java
@@ -4,14 +4,12 @@ import Game.Loot.*;
 import Game.Room.Item;
 
 public class Enemy extends Entity {       //This is an enemy entity
-   private String name;                           //name of enemy entity
    private int damage;                            //damage enemy deals
    private ItemDrop[] lootDrop;                   //array of possible items that defeated monsters can drop
    private String description;                    //Description of enemy
 	
    public Enemy() {
       super ("Default", 1);
-      name = null;
       damage = 0;
       lootDrop = null;
       description = "Debug enemy";
@@ -19,10 +17,17 @@ public class Enemy extends Entity {       //This is an enemy entity
 	
    public Enemy(int hp, String name, int damage, ItemDrop[] drops, String description) {
       super (name, hp);
-      this.name = name;
       this.damage = damage;
       lootDrop = drops;
       this.description = description;
+   }
+	
+   public Enemy(Enemy other)
+   {
+	super(other.name, other.getMaxHealth());
+	this.damage = other.damage;
+	this.lootDrop = other.lootDrop;
+	this.description = other.description;
    }
 	
    public void attack(Entity other) {
