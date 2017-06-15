358fde4ff289ac53099b162bc14958b3b7bcdd62
RaghavSrinivasan <sriraghav45@gmail.com>
2017-06-15 17:24:31 -0400

    Create ItemDrop.java

--- Loot/ItemDrop.java
+++ Loot/ItemDrop.java
@@ -1,3 +1,12 @@
+/*
+Class Name: ItemDrop
+Authors: Michael and Raghav
+Date: June 15, 2017
+School: AY Jackson SS
+Purpose: To package together loot chances and items into one object, to randomly generate items for the player while
+         on expeditions or after defeating an enemy.
+*/
+
 package Game.Loot;
 
 import Game.Room.*;
@@ -6,12 +15,13 @@ import Game.GameMechanics.GameMechanics;
 public class ItemDrop
 {
     private int id;
-    private int min;
-    private int max;
+    private int min; //min amount of items that may be obtained 
+    private int max; //max amount of items that may be obtained
     private double chance;
-    private Item drop;
+    private Item drop; //Item type of the drop
     private String name;
 
+    //This constructor takes in the type of Item in the parameters to instantiate it
     public ItemDrop(Item drop, int min, int max, double chance) {
         id = drop.getItemCode(); //Get itemcode from drop
         name = drop.getName(); //Get name from drop
@@ -21,20 +31,22 @@ public class ItemDrop
         this.chance = chance; //Get chance
     }
     
+    //This constructor takes in the id of the Item in the parameters to instantiate it
     public ItemDrop(int id, int min, int max, double chance){
         this.id = id;
         this.min = min;
         this.max = max;
         this.chance = chance;
-        if (id >=0 && id <=6 || id == 8 || id == 14)
+        //instantiates item type using codes:
+        if (id >=GameMechanics.WOODID && id <=GameMechanics. || id == 8 || id == GameMechanics.TRAPID)
       	{
             drop = new Material(id, 1);
          }
-      	 else if (id >= 9 && id <= 12)
+      	 else if (id >= GameMechanics.FISTID && id <= GameMechanics.STEELSWORDID)
          {
              drop = new Weapon(id, 1, GameMechanics.SWORDDAMAGE[id - 9]);
          }
-         else if (id == 13)
+         else if (id == GameMechanics.BOWID)
          {
              drop = new Weapon(id, 1, GameMechanics.BOWDAMAGE);
          }
@@ -44,6 +56,7 @@ public class ItemDrop
          }
     }
 
+    //This method utilizes the fields of the ItemDrop class to return an item that is randomly generated
     public Item drop() {
         Item newItem = null;
         if (RandomGenerator.trueFalse(chance)) { //If item is dropped
