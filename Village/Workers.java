91e29a2ce2c879d4fb3a88b1fba2b068c55510b5
Michael M. Chang <mchangrh@users.noreply.github.com>
2017-06-15 09:15:59 -0400

    Commented Worker

--- Village/Workers.java
+++ Village/Workers.java
@@ -1,3 +1,10 @@
+/*
+	ICS4U
+	Ian Gu
+	6/2/2017
+	AY Jackson SS
+	This is an abstract class of a worker, with subclasses of each type of worker
+ */
 package Game.Village;
 
 import Game.Room.Resource;
