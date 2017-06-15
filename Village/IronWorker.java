d6bfdb2adebfe8834df764078b694245e05619eb
iangu48 <iangu48@users.noreply.github.com>
2017-06-15 09:44:50 -0400

    Update IronWorker.java

--- Village/IronWorker.java
+++ Village/IronWorker.java
@@ -13,9 +13,11 @@ import Game.Room.*;
 
 public class IronWorker extends Workers {
 
+	 //constants
     public static final Material PLUSIRON = new Material(GameMechanics.IRONID, GameMechanics.MINERIRON);
     public static final Material MINUSCOOKEDMEAT = new Material(GameMechanics.COOKEDMEATID, GameMechanics.MINERMEAT);
 
+    //Returns final resources of each type
     public Resource gatherResources() {
         int workers = getNumWorkers();
         Item[] items = {new Material(PLUSIRON, PLUSIRON.getAmount() * workers), new Material(MINUSCOOKEDMEAT, MINUSCOOKEDMEAT.getAmount() * workers)};
@@ -26,6 +28,7 @@ public class IronWorker extends Workers {
         return "Iron worker";
     }
 
+    //Returns types of resources that gatherResources gives
     public Resource getUnitResource() {
         Item[] items = {new Material(PLUSIRON), new Material(MINUSCOOKEDMEAT)};
         return new Resource(items);
